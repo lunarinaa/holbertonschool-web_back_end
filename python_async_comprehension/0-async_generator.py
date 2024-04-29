674f9e10db42e50f73705f5be980f4dbf20c1ae3
lunarinaa <aydan.akbarova@gmail.com>
2024-04-29 17:23:45 +0400

    return type generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async Generator"""
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Couroutine looping 10 times"""
     for i in range(10):
         await asyncio.sleep(1)
