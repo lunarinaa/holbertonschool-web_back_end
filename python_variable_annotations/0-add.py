7210cf32b4c8fd476124b54120832279978fc116
lunarinaa <aydan.akbarova@gmail.com>
2024-04-29 11:25:40 +0400

    pep fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic annotations - add"""
 
+
 def add(a: float, b: float) -> float:
     """Sum of a and b"""
     return a + b
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Complex types - string and int/float to tuple"""
+"""Complex types """
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return tuple"""
     return (k, float(v**2))
