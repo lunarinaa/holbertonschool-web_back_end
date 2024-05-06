b1a24d3e7f2fb211acda42225158cccd7e6470e6
lunarinaa <aydan.akbarova@gmail.com>
2024-05-06 12:45:54 +0400

    log stats

--- NoSQL/10-update_topics.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Task 10"""
-
-
-def update_topics(mongo_collection, name, topics):
-    """changes all topics of a school document based on the name"""
-    name_n = {"name": name}
-    set_s = {"$set": {"topics": topics}}
-    mongo_collection.update_many(name, set)
