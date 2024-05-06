af3b23175a2f6a296d5938b884b824853025bb83
lunarinaa <aydan.akbarova@gmail.com>
2024-05-06 14:15:14 +0400

    Pep fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,6 +4,6 @@
 
 def index_range(page, page_size):
     """returns tuple of start and end indexses"""
-    start_index = page_size * (page -1)
+    start_index = page_size * (page - 1)
     end_index = start_index + page_size
     return start_index, end_index
