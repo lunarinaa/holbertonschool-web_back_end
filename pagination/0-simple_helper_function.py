3778b11b3f5585e4523ce798daef2026a33a6ebd
lunarinaa <aydan.akbarova@gmail.com>
2024-05-06 14:14:06 +0400

    task 0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 0"""
+
+
+def index_range(page, page_size):
+    """returns tuple of start and end indexses"""
+    start_index = page_size * (page -1)
+    end_index = start_index + page_size
+    return start_index, end_index
