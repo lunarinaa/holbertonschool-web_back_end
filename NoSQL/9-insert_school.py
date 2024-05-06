8a495f050e2f4ba471f7ce282b3b16748173d41b
lunarinaa <aydan.akbarova@gmail.com>
2024-05-06 11:52:37 +0400

    insert

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 9"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+    new = mongo_collection.insert_many(kwargs)
+    return new
