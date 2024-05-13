2c4894296bfff2d8c245b37b12f7b6e9b0129062
lunarinaa <aydan.akbarova@gmail.com>
2024-05-13 21:52:15 +0400

    classrooms

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
