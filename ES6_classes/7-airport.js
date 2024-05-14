75ec4dd523f31c0c3b18fb1fb228437ea683d9d1
lunarinaa <aydan.akbarova@gmail.com>
2024-05-14 13:10:33 +0400

    7-airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default class Airport {
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+    }
+    toString() {
+        return `[object ${this._code}]`
+    }
+}
