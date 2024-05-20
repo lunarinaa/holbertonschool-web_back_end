371c15911eb6418302665040e4eb813d7b5b937d
lunarinaa <aydan.akbarova@gmail.com>
2024-05-20 10:51:29 +0400

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise
+    .allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
+    .then(results => results.map(result => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
