4e67a68f316baf7dcd613cbeca438110bda39c8a
lunarinaa <aydan.akbarova@gmail.com>
2024-05-20 12:20:25 +0400

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,11 +4,11 @@ import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise
-    .allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
-    .then(results => (
-        results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : String(result.reason),
-          }))
-        ));
-    }
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? o.value : String(result.reason),
+      }))
+    ));
+}
