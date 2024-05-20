78ef337fb722e3da0e5afea8072b13032c3d6c79
lunarinaa <aydan.akbarova@gmail.com>
2024-05-20 12:25:26 +0400

    Revert "checking code"
    This reverts commit cf4c1fb9eab70a2a418879650fe03cf7fc60c210.

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    .then((results) => (
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? o.value : String(result.reason),
       }))
     ));
 }
