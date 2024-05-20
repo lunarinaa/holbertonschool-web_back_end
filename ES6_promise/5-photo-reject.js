14b0128c2f18251d466ef76794b862a9c26051c9
lunarinaa <aydan.akbarova@gmail.com>
2024-05-20 12:16:02 +0400

    semistandard fix

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
-export default function uploadPhoto(filename) {
-    return Promise.reject(new Error(`${filename} cannot be processed`))
+export default function uploadPhoto(fileName) {
+    return Promise.reject(new Error(`${fileName} cannot be processed`))
 }
